1b4a403948677b39ddddef6d1b800db8dc71fc77
mo <moritz.hausmann@detecon.com>
2025-04-11 21:36:32 +0200

    refactor: now REAL

---
+++ real_dataclasses/__init__.py
@@ -0,0 +1,2 @@
+from tasks.email_task import EmailTask
+from tasks.username_task import UsernameTask
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dataclasses",
-    version="0.1",
-    description="Dataclasses for the a-real OSINT toolset",
+    name="real_dataclasses",
+    version="0.2",
+    description="Real Dataclasses for the a-real OSINT toolset",
     long_description=long_description,
     install_requires=[],
     long_description_content_type="text/markdown",
     url="https://github.com/a-real-osint/dataclassses",
-    packages=["dataclasses"])
+    packages=["real_dataclasses"])
